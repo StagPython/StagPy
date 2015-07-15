62a8ed89413be45255acf33eafcb9564cc5af8f9
amorison <amorison@users.noreply.github.com>
2015-07-15 12:11:09 +0200

    _readbin() abstracts unpacking of bin data

--- stag.py
+++ stag.py
@@ -12,19 +12,27 @@ class ReadStagyyData:
         self.ti_fn = ti_fn
         self.file_format = 'l'
 
-        if ti_fn < 10000:
-            mylist = list(str(10000 + ti_fn))
-            mylist[0] = '0'
-            number_string = ''.join(mylist)
-        else:
-            number_string = str(ti_fn)
-        self.fullname = fpath + fname + '_' + par_type + \
-            number_string  # name of the file to read
+        number_string = '{:05d}'.format(ti_fn)
 
-        if (par_type == 't') or (par_type == 'eta') or (par_type == 'rho') or (par_type == 'str') or (par_type == 'age'):
+        # name of the file to read
+        self.fullname = fpath + fname + '_' + par_type + number_string
+
+        if par_type in ('t', 'eta', 'rho', 'str', 'age'):
             self.nval = 1
         elif par_type == 'vp':
             self.nval = 4
+
+    def _readbin(self, fid, fmt, nwords=1, nbytes=4):
+        """Read n words of n bytes with fmt format
+        from the fid file, update self.byte_offset.
+        Return a tuple of elements if more
+        than one element."""
+
+        elts = struct.unpack(fmt*nwords, fid.read(nwords*nbytes))
+        if len(elts) == 1:
+            elts = elts[0]
+        self.byte_offset += nwords * nbytes
+        return elts
 
     #==========================================================================
     # READ HEADER
@@ -32,12 +40,11 @@ class ReadStagyyData:
     def catch_header(self):
         fid = open(self.fullname, 'rb')  # open file
         self.byte_offset = 0
-        self.nmagic = struct.unpack('i', fid.read(4))[0]  # Version
-        self.byte_offset = self.byte_offset + 4
+        self.nmagic = self._readbin(fid, 'i')  # Version
 
         # check nb components
         if (self.nmagic < 100 and self.nval > 1) or (self.nmagic > 300 and self.nval == 1):
-            error('wrong number of components in field')
+            raise ValueError('wrong number of components in field')
 
         nnmagic = self.nmagic % 100
         if nnmagic >= 9 and self.nval == 4:
@@ -46,57 +53,37 @@ class ReadStagyyData:
             self.xyp = 0
 
         # total number of values in the latitude direction
-        self.nthtot = struct.unpack('i', fid.read(4))[0]
-        self.byte_offset = self.byte_offset + 4
+        self.nthtot = self._readbin(fid, 'i')
         # total number of values in the longitude direction
-        self.nphtot = struct.unpack('i', fid.read(4))[0]
-        self.byte_offset = self.byte_offset + 4
+        self.nphtot = self._readbin(fid, 'i')
         # total number of values in the radius direction
-        self.nrtot = struct.unpack('i', fid.read(4))[0]
-        self.byte_offset = self.byte_offset + 4
+        self.nrtot = self._readbin(fid, 'i')
         # of blocks, 2 for yinyan
-        self.nblocks = struct.unpack('i', fid.read(4))[0]
-        self.byte_offset = self.byte_offset + 4
-        self.aspect = np.zeros(2)
-        self.aspect[0] = struct.unpack('f', fid.read(4))[0]  # Aspect ratio
-        self.aspect[1] = struct.unpack('f', fid.read(4))[0]  # Aspect ratio
-        self.byte_offset = self.byte_offset + 4 * 2
+        self.nblocks = self._readbin(fid, 'i')
+
+        # Aspect ratio
+        self.aspect = self._readbin(fid, 'f', 2)
+        self.aspect = np.array(self.aspect)
+
         # Number of parallel subdomains
-        self.nnth = struct.unpack('i', fid.read(4))[0]
-        self.byte_offset = self.byte_offset + 4
         # in the th,ph,r and b directions
-        self.nnph = struct.unpack('i', fid.read(4))[0]
-        self.byte_offset = self.byte_offset + 4
-        self.nnr = struct.unpack('i', fid.read(4))[0]
-        self.byte_offset = self.byte_offset + 4
-        self.nnb = struct.unpack('i', fid.read(4))[0]
-        self.byte_offset = self.byte_offset + 4
+        self.nnth = self._readbin(fid, 'i')
+        self.nnph = self._readbin(fid, 'i')
+        self.nnr = self._readbin(fid, 'i')
+        self.nnb = self._readbin(fid, 'i')
 
         self.nr2 = self.nrtot * 2 + 1
-        self.rg = struct.unpack(
-            'f' * self.nr2, fid.read(4 * self.nr2))  # r-coordinates
-        self.byte_offset = self.byte_offset + 4 * self.nr2
+        self.rg = self._readbin(fid, 'f', self.nr2)  # r-coordinates
 
-        self.rcmb = struct.unpack('f', fid.read(4))[0]  # radius of the cmb
-        self.byte_offset = self.byte_offset + 4
-        self.ti_step = struct.unpack('i', fid.read(4))[0]
-        self.byte_offset = self.byte_offset + 4
-        self.ti_ad = struct.unpack('f', fid.read(4))[0]
-        self.byte_offset = self.byte_offset + 4
-        self.erupta_total = struct.unpack('f', fid.read(4))[0]
-        self.byte_offset = self.byte_offset + 4
-        self.botT_val = struct.unpack('f', fid.read(4))[0]
-        self.byte_offset = self.byte_offset + 4
+        self.rcmb = self._readbin(fid, 'f')  # radius of the cmb
+        self.ti_step = self._readbin(fid, 'i')
+        self.ti_ad = self._readbin(fid, 'f')
+        self.erupta_total = self._readbin(fid, 'f')
+        self.botT_val = self._readbin(fid, 'f')
 
-        self.th_coord = struct.unpack(
-            'f' * self.nthtot, fid.read(4 * self.nthtot))  # th-coordinates
-        self.byte_offset = self.byte_offset + 4 * self.nthtot
-        self.ph_coord = struct.unpack(
-            'f' * self.nphtot, fid.read(4 * self.nphtot))  # ph-coordinates
-        self.byte_offset = self.byte_offset + 4 * self.nphtot
-        self.r_coord = struct.unpack(
-            'f' * self.nrtot, fid.read(4 * self.nrtot))  # r-coordinates
-        self.byte_offset = self.byte_offset + 4 * self.nrtot
+        self.th_coord = self._readbin(fid, 'f', self.nthtot)  # th-coordinates
+        self.ph_coord = self._readbin(fid, 'f', self.nphtot)  # ph-coordinates
+        self.r_coord = self._readbin(fid, 'f', self.nrtot)  # r-coordinates
 
         fid.close()
 
@@ -124,8 +111,7 @@ class ReadStagyyData:
                 for iphc in np.arange(self.nnph):
                     for ithc in np.arange(self.nnth):
                         # read the data for this CPU
-                        fileContent = struct.unpack(
-                            'f' * npi, fid.read(4 * npi))
+                        fileContent = self._readbin(fid, 'f', npi)
                         data_CPU = np.array(fileContent)
                         # Create a 3D matrix from these data
                         #data_CPU_3D = data_CPU.reshape((nth,nph,nr,nb))
@@ -161,7 +147,7 @@ class ReadStagyyData:
         # the number of values per 'read' block
         npi = (nth + self.xyp) * (nph + self.xyp) * nr * nb * self.nval
 
-        self.scalefac = struct.unpack('f', fid.read(4))[0]
+        self.scalefac = self._readbin(fid, 'f')
 
         vx_3D = np.zeros(
             (self.nblocks, self.nrtot, self.nphtot + self.xyp, self.nthtot + self.xyp))
@@ -178,8 +164,7 @@ class ReadStagyyData:
                 for iphc in np.arange(self.nnph):
                     for ithc in np.arange(self.nnth):
                         # read the data for this CPU
-                        fileContent = struct.unpack(
-                            'f' * npi, fid.read(4 * npi))
+                        fileContent = self._readbin(fid, 'f', npi)
                         data_CPU = np.array(fileContent)
                         data_CPU = data_CPU * self.scalefac
                         # Create a 3D matrix from these data
