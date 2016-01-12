76dd93f625aa7ee03607145fda5382024237f0ab
Adrien Morison <adrien.morison@gmail.com>
2016-01-12 15:09:15 +0100

    n refers to viscosity instead of v
    could lead to confusion with velocity

--- constants.py
+++ constants.py
@@ -10,7 +10,7 @@ Var = namedtuple('Var', ['par', 'name', 'func'])
 VAR_LIST = OrderedDict((
     ('t', Var('t', 'Temperature', misc.takefield(0))),
     ('c', Var('c', 'Composition', misc.takefield(0))),
-    ('v', Var('eta', 'Viscosity', misc.takefield(0))),
+    ('n', Var('eta', 'Viscosity', misc.takefield(0))),
     ('d', Var('rho', 'Density', misc.takefield(0))),
     ('p', Var('vp', 'Pressure', misc.takefield(3))),
     ('s', Var('vp', 'Stream function', processing.calc_stream))
