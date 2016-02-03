Fix stream function calculation

stagpy/field.py
@@ -15,7 +15,8 @@ def plot_scalar(args, stgdat, var):
     # adding a row at the end to have continuous field
     if stgdat.geom == 'annulus':
         if stgdat.par_type == 'vp':
-            fld = fld[:, :, 0]
+            if var!='s':
+                fld = fld[:, :, 0]
         else:
             newline = fld[:, 0, 0]
             fld = np.vstack([fld[:, :, 0].T, newline]).T

stagpy/stagdata.py
@@ -145,9 +145,9 @@ class BinData:
         only make sense with vp fields
         """
         # should add test if vp fields or not
-        vphi = self.fields['vy'][:, :, 0]
+        vphi = self.fields['v'][:, :, 0]
         vph2 = -0.5 * (vphi + np.roll(vphi, 1, 1))  # interpolate to the same phi
-        v_r = self.fields['vz'][:, :, 0]
+        v_r = self.fields['w'][:, :, 0]
         n_r, nph = np.shape(v_r)
         stream = np.zeros(np.shape(vphi))
         # integrate first on phi