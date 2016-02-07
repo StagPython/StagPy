b8c35ac3439a4c08a028095f8c4f7603ea1dead1
Adrien Morison <adrien.morison@gmail.com>
2016-02-07 20:13:46 +0100

    Clean time_cmd a bit

--- stagpy/time_series.py
+++ stagpy/time_series.py
@@ -23,16 +23,6 @@ def time_cmd(args):
     lwdth = args.linewidth
     ftsz = args.fontsize
 
-    if not args.par_nml:
-        print('No par file found. Abort.')
-        sys.exit()
-
-    spherical = args.par_nml['geometry']['shape'].lower() == 'spherical'
-    if spherical:
-        rcmb = args.par_nml['geometry']['r_cmb']
-    else:
-        rcmb = 0.
-
     rab = args.par_nml['refstate']['Ra0']
     rah = args.par_nml['refstate']['Rh']
     botpphase = args.par_nml['boundaries']['BotPphase']
@@ -41,13 +31,16 @@ def time_cmd(args):
     colnames, data = time_data.colnames, time_data.data
     ntot = len(data)
 
+    spherical = args.par_nml['geometry']['shape'].lower() == 'spherical'
     if spherical:
+        rcmb = args.par_nml['geometry']['r_cmb']
         rmin = rcmb
         rmax = rmin + 1
         coefb = 1  # rb**2*4*pi
         coefs = (rmax / rmin)**2  # *4*pi
         volume = rmin * (1 - (rmax / rmin)**3) / 3  # *4*pi/3
     else:
+        rcmb = 0.
         coefb = 1.
         coefs = 1.
         volume = 1.
@@ -87,57 +80,56 @@ def time_cmd(args):
 
     plt.savefig("flux_time.pdf", format='PDF')
 
-    if args.compstat:
-        coords = []
-        print('right click to select starting time of statistics computations')
-        # Simple mouse click function to store coordinates
+    if not args.compstat:
+        return None
 
-        def onclick(event):
-            """get position and button from mouse click"""
-            ixc, iyc = event.xdata, event.ydata
-            button = event.button
-            # assign global variable to access outside of function
-            if button == 3:
-                coords.append((ixc, iyc))
-                # Disconnect after 1 clicks
-            if len(coords) == 1:
-                fig.canvas.mpl_disconnect(cid)
-                plt.close(1)
-            return
-        # Call click func
-        cid = fig.canvas.mpl_connect('button_press_event', onclick)
+    coords = []
+    print('right click to select starting time of statistics computations')
 
+    def onclick(event):
+        """get position and button from mouse click"""
+        ixc, iyc = event.xdata, event.ydata
+        button = event.button
+        # assign global variable to access outside of function
+        if button == 3:
+            coords.append((ixc, iyc))
+            # Disconnect after 1 clicks
+        if len(coords) == 1:
+            fig.canvas.mpl_disconnect(cid)
+            plt.close(1)
+        return
+
+    # Call click func
+    cid = fig.canvas.mpl_connect('button_press_event', onclick)
     plt.show()
 
     moy = []
     rms = []
     ebal = []
     rms_ebal = []
-    if args.compstat:
-        ch1 = np.where(time == (find_nearest(time, coords[0][0])))
+    ch1 = np.where(time == (find_nearest(time, coords[0][0])))
 
-        print('Statistics computed from t =' + str(time[ch1[0][0]]))
-        for num in range(2, len(colnames)):
-            moy.append(np.trapz(data[ch1[0][0]:ntot - 1, num],
-                                x=time[ch1[0][0]:ntot - 1]) /
-                       (time[ntot - 1] - time[ch1[0][0]]))
-            rms.append(sqrt(np.trapz((data[ch1[0][0]:ntot - 1, num] -
-                                      moy[num - 2])**2,
-                                     x=time[ch1[0][0]:ntot - 1]) /
-                            (time[ntot - 1] - time[ch1[0][0]])))
-            print(colnames[num], '=', moy[num - 2], 'pm', rms[num - 2])
-        ebal.append(np.trapz(ebalance[ch1[0][0] - 1:ntot - 3],
-                             x=time[ch1[0][0]:ntot - 2]) /
-                    (time[ntot - 2] - time[ch1[0][0]]))
-        rms_ebal.append(sqrt(np.trapz(
-                             (ebalance[ch1[0][0] - 1:ntot - 3] - ebal)**2,
-                             x=time[ch1[0][0]:ntot - 2]) /
-                             (time[ntot - 2] - time[ch1[0][0]])))
-        print('Energy balance', ebal, 'pm', rms_ebal)
-        results = moy + ebal + rms + rms_ebal
-        fich = open('Stats.dat', 'w')
-        fich.write("%10.5e %10.5e %10.5e " % (rab, rah, botpphase))
+    print('Statistics computed from t =' + str(time[ch1[0][0]]))
+    for num in range(2, len(colnames)):
+        moy.append(np.trapz(data[ch1[0][0]:ntot - 1, num],
+                            x=time[ch1[0][0]:ntot - 1]) /
+                   (time[ntot - 1] - time[ch1[0][0]]))
+        rms.append(sqrt(np.trapz((data[ch1[0][0]:ntot - 1, num] -
+                                  moy[num - 2])**2,
+                                 x=time[ch1[0][0]:ntot - 1]) /
+                        (time[ntot - 1] - time[ch1[0][0]])))
+        print(colnames[num], '=', moy[num - 2], 'pm', rms[num - 2])
+    ebal.append(np.trapz(ebalance[ch1[0][0] - 1:ntot - 3],
+                         x=time[ch1[0][0]:ntot - 2]) /
+                (time[ntot - 2] - time[ch1[0][0]]))
+    rms_ebal.append(sqrt(np.trapz(
+                         (ebalance[ch1[0][0] - 1:ntot - 3] - ebal)**2,
+                         x=time[ch1[0][0]:ntot - 2]) /
+                         (time[ntot - 2] - time[ch1[0][0]])))
+    print('Energy balance', ebal, 'pm', rms_ebal)
+    results = moy + ebal + rms + rms_ebal
+    with open('Stats.dat', 'w') as out_file:
+        out_file.write("%10.5e %10.5e %10.5e " % (rab, rah, botpphase))
         for item in results:
-            fich.write("%10.5e " % item)
-        fich.write("\n")
-        fich.close()
+            out_file.write("%10.5e " % item)
+        out_file.write("\n")
