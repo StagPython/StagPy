33c1cf58f3a9044d7832a94380fe48f81945bd80
Adrien Morison <adrien.morison@gmail.com>
2017-01-24 14:13:57 +0100

    More robust __version__ number

--- stagpy/__init__.py
+++ stagpy/__init__.py
@@ -1,4 +1,11 @@
 """define current version"""
-from setuptools_scm import get_version
 
-__version__ = get_version()
+from setuptools_scm import get_version
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_version(root='..', relative_to=__file__)
+except LookupError:
+    __version__ = get_distribution('stagpy').version
+except (DistributionNotFound, ValueError):
+    __version__ = 'unknown'
