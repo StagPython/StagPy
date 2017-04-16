44e64c4f0c0296015fb124cddf87bea2188559fe
Adrien Morison <adrien.morison@gmail.com>
2017-04-16 23:34:32 +0200

    Handle SIGINT in a clean way

--- stagpy/__main__.py
+++ stagpy/__main__.py
@@ -1,11 +1,20 @@
 # PYTHON_ARGCOMPLETE_OK
 """The stagpy module is callable"""
 
+import signal
+import sys
 from . import config
+
+
+def sigint_handler(*_):
+    """SIGINT handler"""
+    print('\nSo long, and thanks for all the fish.')
+    sys.exit()
 
 
 def main():
     """StagPy entry point"""
+    signal.signal(signal.SIGINT, sigint_handler)
     args = config.parse_args()
     args.func(args)
 
