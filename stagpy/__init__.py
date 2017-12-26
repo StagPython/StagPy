12388803848f9101fc46aa2fb5fe81036c0b2a2e
Adrien Morison <adrien.morison@gmail.com>
2017-12-27 00:41:49 +0100

    Group plotting imports in a function in __init__

--- stagpy/__init__.py
+++ stagpy/__init__.py
@@ -5,6 +5,19 @@ from pkg_resources import get_distribution, DistributionNotFound
 import importlib
 from . import config
 
+
+def _load_mpl():
+    """Load matplotlib and set some configuration"""
+    mpl = importlib.import_module('matplotlib')
+    if conf.core.matplotback:
+        mpl.use(conf.core.matplotback)
+    plt = importlib.import_module('matplotlib.pyplot')
+    if conf.core.useseaborn:
+        importlib.import_module('seaborn')
+    if conf.core.xkcd:
+        plt.xkcd()
+
+
 try:
     __version__ = get_version(root='..', relative_to=__file__)
 except LookupError:
@@ -14,11 +27,4 @@ except (DistributionNotFound, ValueError):
 
 conf = config.StagpyConfiguration()
 
-mpl = importlib.import_module('matplotlib')
-if conf.core.matplotback:
-    mpl.use(conf.core.matplotback)
-plt = importlib.import_module('matplotlib.pyplot')
-if conf.core.useseaborn:
-    importlib.import_module('seaborn')
-if conf.core.xkcd:
-    plt.xkcd()
+_load_mpl()
