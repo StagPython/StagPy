4cc113b322ddb02c4ed35d193a9c29c32f434162
Adrien Morison <adrien.morison@gmail.com>
2017-12-28 02:33:55 +0100

    Order in autodoc set to 'bysource'
    This order is more natural than 'alphabetical'

--- docs/conf.py
+++ docs/conf.py
@@ -56,6 +56,7 @@ extensions = [
     'sphinx.ext.autodoc',
 ]
 
+autodoc_member_order = 'bysource'
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
