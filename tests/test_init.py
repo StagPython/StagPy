12956febe15e38c8b39edb4e268aa27228a4249f
Adrien Morison <adrien.morison@gmail.com>
2018-01-22 00:08:03 +0100

    Remove unecessary parentheses with assert stmt

--- tests/test_init.py
+++ tests/test_init.py
@@ -5,7 +5,7 @@ import helpers
 def test_init_config_None():
     stagpy.conf.core.path = 'some_path'
     helpers.reset_config()
-    assert(stagpy.conf.core.path == './')
+    assert stagpy.conf.core.path == './'
 
 def test_version_format():
-    assert(stagpy.__version__ == safe_version(stagpy.__version__))
+    assert stagpy.__version__ == safe_version(stagpy.__version__)
