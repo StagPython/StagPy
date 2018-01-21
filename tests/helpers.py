d206b42afc9304ff42b357044c29c7dedbea1f15
Adrien Morison <adrien.morison@gmail.com>
2018-01-21 19:43:35 +0100

    Add test of init_config

---
+++ tests/helpers.py
@@ -0,0 +1,5 @@
+import stagpy
+
+def reset_config():
+    """Reset stagpy.conf to default values."""
+    stagpy.init_config(None)
--- tests/test_version.py
+++
@@ -1,5 +0,0 @@
-from pkg_resources import safe_version
-import stagpy
-
-def test_version_format():
-    assert(stagpy.__version__ == safe_version(stagpy.__version__))
