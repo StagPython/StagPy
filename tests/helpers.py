76f6e6c19b90979e3e3b50259f07a1eab87cb8a6
Adrien Morison <adrien.morison@gmail.com>
2018-01-22 01:08:13 +0100

    Add tests of stagpy.commands output

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,4 +1,8 @@
+import pathlib
 import stagpy
+
+repo_dir = pathlib.Path(__file__).parent.parent.resolve()
+ra1e5_dir = repo_dir / 'Examples' / 'ra-100000'
 
 def reset_config():
     """Reset stagpy.conf to default values."""
---
+++ tests/test_commands.py
@@ -0,0 +1,33 @@
+import re
+import helpers
+import stagpy.commands
+
+def test_info_cmd(capsys):
+    stagpy.conf.core.path = helpers.ra1e5_dir
+    stagpy.commands.info_cmd()
+    output = capsys.readouterr()
+    expected = re.compile(
+        r'^StagYY run in.*\nLast timestep:\n.*\n.*\n.*\nLast snapshot .*\n'
+        r'.*\n.*\n.*\n$')
+    assert expected.fullmatch(output.out)
+    helpers.reset_config()
+
+def test_var_cmd(capsys):
+    stagpy.commands.var_cmd()
+    output = capsys.readouterr()
+    expected = re.compile(
+        r'field:\n.*\nrprof:\n.*\ntime:\n.*\nplates:\n.*$',
+        flags=re.DOTALL)
+    assert expected.fullmatch(output.out)
+
+def test_version_cmd(capsys):
+    stagpy.commands.version_cmd()
+    output = capsys.readouterr()
+    expected = 'stagpy version: {}\n'.format(stagpy.__version__)
+    assert output.out == expected
+
+def test_config_cmd(capsys):
+    stagpy.commands.config_cmd()
+    output = capsys.readouterr()
+    expected = '(c|f): available only as CLI argument/in the config file'
+    assert output.out.startswith(expected)
