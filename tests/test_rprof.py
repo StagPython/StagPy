aca344da71a35b61c0cb60abfe5b5ecad5f7073d
Adrien Morison <adrien.morison@gmail.com>
2018-01-24 00:32:23 +0100

    Add tests of stagpy.rprof

---
+++ tests/test_rprof.py
@@ -0,0 +1,12 @@
+import stagpy.rprof, stagpy.phyvars
+
+def test_get_rprof(step):
+    prof, rad, meta = stagpy.rprof.get_rprof(step, 'Tmean')
+    assert rad is None
+    assert prof.shape == (step.rprof.shape[0],)
+    assert meta == stagpy.phyvars.RPROF['Tmean']
+
+def test_get_rprof_extra(step):
+    prof, rad, meta = stagpy.rprof.get_rprof(step, 'diff')
+    assert prof.shape == rad.shape == (step.rprof.shape[0] + 1,)
+    assert isinstance(meta, stagpy.phyvars.Varr)
