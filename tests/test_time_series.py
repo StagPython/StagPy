6ccd72c18be88c4ec299e1d64d40c060b0bc1ddc
Adrien Morison <adrien.morison@gmail.com>
2018-01-24 00:38:36 +0100

    Add tests of stagpy.time_series

---
+++ tests/test_time_series.py
@@ -0,0 +1,14 @@
+import stagpy.phyvars, stagpy.time_series
+
+def test_get_time_series(sdat):
+    series, time, meta = stagpy.time_series.get_time_series(
+        sdat, 'Tmean', None, None)
+    assert time is None
+    assert series.shape == (sdat.tseries.shape[0],)
+    assert meta == stagpy.phyvars.TIME['Tmean']
+
+def test_get_time_series_extra(sdat):
+    series, time, meta = stagpy.time_series.get_time_series(
+        sdat, 'dTdt', None, None)
+    assert series.shape == time.shape == (sdat.tseries.shape[0] - 1,)
+    assert isinstance(meta, stagpy.phyvars.Vart)
