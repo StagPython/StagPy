dc3b6cee08c8501d01a9099aeddcccdc41b8c7b9
Adrien Morison <adrien.morison@gmail.com>
2018-11-01 22:52:44 +0100

    Remove pylint related comments

--- stagpy/__init__.py
+++ stagpy/__init__.py
@@ -71,10 +71,8 @@ except (DistributionNotFound, ValueError):
 
 _CONF_FILES = ([config.CONFIG_FILE, config.CONFIG_LOCAL]
                if not _env('STAGPY_NO_CONFIG') else [])
-# pylint: disable=invalid-name
 conf = ConfigurationManager.from_dict_(config.CONF_DEF)
 conf.set_config_files_(*_CONF_FILES)
-# pylint: enable=invalid-name
 PARSING_OUT = conf.read_configs_()
 
 _load_mpl()
