f920f46361a144d9da34f0c3ce81bf1aa75e5a94
Adrien Morison <adrien.morison@gmail.com>
2018-11-17 23:54:30 +0100

    Add time and rprof CLI tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,32 +2,62 @@ import pytest
 import subprocess
 
 
-@pytest.fixture(params=[('ra-100000', 5), ('annulus', 100)])
+@pytest.fixture(params=[('ra-100000', 5, 1000), ('annulus', 100, 3000)])
 def dir_isnap(request, repo_dir):
-    return repo_dir / 'Examples' / request.param[0], request.param[1]
+    return (repo_dir / 'Examples' / request.param[0],
+            request.param[1], request.param[2])
 
 
 @pytest.fixture(params=[
-    ('stagpy field -o=T', ['stagpy_T{:05d}.pdf']),
+    ('stagpy field', ['stagpy_T_stream{:05d}.pdf']),
     ('stagpy field -o=T,v3', ['stagpy_T{:05d}.pdf',
                               'stagpy_v3{:05d}.pdf']),
 ])
-def cmd_line(request):
-    return request.param
-
-
-@pytest.fixture
-def all_cmd(cmd_line, dir_isnap):
-    cmd = cmd_line[0]
+def all_cmd_field(request, dir_isnap):
+    cmd = request.param[0]
     cmd += ' -p={}'.format(dir_isnap[0])
     expected_files = []
-    for expfile in cmd_line[1]:
+    for expfile in request.param[1]:
         expected_files.append(expfile.format(dir_isnap[1]))
     return cmd, expected_files
 
 
-def test_cli(all_cmd, tmpdir):
-    subprocess.run(all_cmd[0] + ' -n={}/stagpy'.format(tmpdir), shell=True)
-    produced_files = tmpdir.listdir(sort=True)
-    expected_files = [tmpdir / expfile for expfile in all_cmd[1]]
+@pytest.fixture(params=[
+    ('stagpy rprof', ['stagpy_rprof_Tmean_{}.pdf']),
+    ('stagpy rprof -o=Tmean,vzabs', ['stagpy_rprof_Tmean_vzabs_{}.pdf']),
+])
+def all_cmd_rprof(request, dir_isnap):
+    cmd = request.param[0]
+    cmd += ' -p={}'.format(dir_isnap[0])
+    expected_files = []
+    for expfile in request.param[1]:
+        expected_files.append(expfile.format(dir_isnap[2]))
+    return cmd, expected_files
+
+
+@pytest.fixture(params=[
+    ('stagpy time -o Tmean.Nutop', ['stagpy_time_Tmean_Nutop.pdf']),
+])
+def all_cmd_time(request, dir_isnap):
+    cmd = request.param[0]
+    cmd += ' -p={}'.format(dir_isnap[0])
+    return cmd, request.param[1]
+
+
+def helper_test_cli(all_cmd, tmp):
+    subprocess.run(all_cmd[0] + ' -n={}/stagpy'.format(tmp), shell=True)
+    produced_files = tmp.listdir(sort=True)
+    expected_files = [tmp / expfile for expfile in all_cmd[1]]
     assert produced_files == expected_files
+
+
+def test_field_cli(all_cmd_field, tmpdir):
+    helper_test_cli(all_cmd_field, tmpdir)
+
+
+def test_rprof_cli(all_cmd_rprof, tmpdir):
+    helper_test_cli(all_cmd_rprof, tmpdir)
+
+
+def test_time_cli(all_cmd_time, tmpdir):
+    helper_test_cli(all_cmd_time, tmpdir)
