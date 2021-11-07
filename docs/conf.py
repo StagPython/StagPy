b6f683909f2bc64c4d87ff67fadd3abc629287fe
Adrien Morison <adrien.morison@gmail.com>
2021-11-07 14:42:00 +0000

    No longer mock out dependency on readthedocs
    Downloading these packages does not dominate documentation build
    time.

--- docs/conf.py
+++ docs/conf.py
@@ -27,19 +27,6 @@ else:
 # do not try to create/read config files
 os.environ['STAGPY_ISOLATED'] = 'True'
 
-# -- To mock out heavy dependencies ---------------------------------------
-from unittest.mock import MagicMock
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return MagicMock()
-
-MOCK_MODULES = ['numpy', 'scipy', 'scipy.signal', 'pandas', 'h5py',
-                'matplotlib', 'matplotlib.patches', 'matplotlib.pyplot',
-                'mpl_toolkits.axes_grid1']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-import os
-
 from setuptools import setup
 
 with open('README.rst') as rdm:
@@ -9,19 +7,12 @@ DEPENDENCIES = [
     'loam>=0.3.1',
     'f90nml>=1.3.1',
     'setuptools_scm>=6.2',
-]
-HEAVY = [
     'numpy>=1.19',
     'scipy>=1.5',
     'pandas>=1.1',
     'h5py>=3.1',
     'matplotlib>=3.3',
 ]
-
-ON_RTD = os.environ.get('READTHEDOCS', None) == 'True'
-# heavy dependencies are mocked out on Read the Docs
-if not ON_RTD:
-    DEPENDENCIES.extend(HEAVY)
 
 setup(
     name='stagpy',
