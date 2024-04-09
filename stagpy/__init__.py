e4822ecda1552d4928b6f202dfbbad260221b3c4
Adrien Morison <adrien.morison@gmail.com>
2024-04-09 11:41:10 +0100

    env var STAGPY_DEBUG enabled if it is set
    StagPy would previously check that it is set to a "truthy" value,
    which added unneeded complexity.

--- stagpy/__init__.py
+++ stagpy/__init__.py
@@ -4,13 +4,9 @@ StagPy is both a CLI tool and a powerful Python library. See the
 documentation at
 https://stagpy.readthedocs.io/en/stable/
 
-When using the CLI interface, if the environment variable STAGPY_DEBUG is set
-to a truthy value, warnings are issued normally and StagpyError are raised.
-Otherwise, warnings are ignored and only a short form of encountered
-StagpyErrors is printed.
-
-Truthy values for environment variables are 'true', 't', 'yes', 'y', 'on', '1',
-and uppercase versions of those.
+When using the CLI interface, warnings are ignored and only a short form of
+encountered StagpyErrors is printed. Set the environment variable STAGPY_DEBUG
+to issue warnings normally and raise StagpyError.
 """
 
 from __future__ import annotations
@@ -24,13 +20,7 @@ if typing.TYPE_CHECKING:
     from typing import Any, NoReturn
 
 
-def _env(var: str) -> bool:
-    """Return whether var is set to True."""
-    val = os.getenv(var, default="").lower()
-    return val in ("true", "t", "yes", "y", "on", "1")
-
-
-DEBUG = _env("STAGPY_DEBUG")
+DEBUG = os.getenv("STAGPY_DEBUG") is not None
 
 
 def sigint_handler(*_: Any) -> NoReturn:
