38f174e11f0bb3dd0f026582059ca3a85d6f10e9
Adrien Morison <adrien.morison@gmail.com>
2024-04-15 11:35:37 +0100

    gen_ref_pages: minor improvements

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -11,21 +11,20 @@ src = root
 
 for path in sorted((src / "stagpy").rglob("*.py")):
     module_path = path.relative_to(src).with_suffix("")
-    doc_path = path.relative_to(src).with_suffix(".md")
+    doc_path = module_path.with_suffix(".md")
 
     parts = module_path.parts
 
     if parts[-1] == "__init__":
         parts = parts[:-1]
-        doc_path = doc_path.with_name("index.md")
-
-    full_doc_path = Path("reference", doc_path)
+        doc_path = doc_path.with_stem("index")
 
     if any(p.startswith("_") for p in parts):
         continue
 
     nav[parts] = doc_path.as_posix()
 
+    full_doc_path = Path("reference", doc_path)
     with mkdocs_gen_files.open(full_doc_path, "w") as fd:
         identifier = ".".join(parts)
         print("::: " + identifier, file=fd)
