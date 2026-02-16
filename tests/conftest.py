4aa5b12809f3bb19319b04992218c99e77981222
Adrien Morison <adrien.morison@gmail.com>
2026-02-16 18:19:27 +0000

    add test data with hdf5 output

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,14 +10,54 @@ def repo_dir() -> Path:
     return Path(__file__).parent.parent.resolve()
 
 
-@fixture(scope="session", params=["ra-100000", "annulus"])
+@fixture(
+    scope="session",
+    params=[
+        "ra-100000",
+        "annulus",
+        "blankenbach/serial",
+        "blankenbach/parallel",
+    ],
+)
 def example_dir(request: FixtureRequest, repo_dir: Path) -> Path:
+    return repo_dir / "Examples" / request.param
+
+
+@fixture(
+    scope="session",
+    params=[
+        "ra-100000",
+        "annulus",
+    ],
+)
+def example_legacy_path(request: FixtureRequest, repo_dir: Path) -> Path:
+    return repo_dir / "Examples" / request.param
+
+
+@fixture(
+    scope="session",
+    params=[
+        "blankenbach/serial",
+        "blankenbach/parallel",
+    ],
+)
+def example_h5_path(request: FixtureRequest, repo_dir: Path) -> Path:
     return repo_dir / "Examples" / request.param
 
 
 @fixture(scope="module")
 def sdat(example_dir: Path) -> StagyyData:
     return StagyyData(example_dir)
+
+
+@fixture(scope="module")
+def sdat_legacy(example_legacy_path: Path) -> StagyyData:
+    return StagyyData(example_legacy_path)
+
+
+@fixture(scope="module")
+def sdat_h5(example_h5_path: Path) -> StagyyData:
+    return StagyyData(example_h5_path)
 
 
 @fixture(scope="module")
--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -1,10 +1,11 @@
-import pathlib
+from pathlib import Path
 
 from stagpy import stagyyparsers as prs
 from stagpy.stagyydata import StagyyData
 
 
-def test_time_series_prs(sdat: StagyyData) -> None:
+def test_time_series_prs(sdat_legacy: StagyyData) -> None:
+    sdat = sdat_legacy
     names = ["aa", "bb", "cc"]
     data = prs.time_series(sdat.filename("time.dat"), list(names))
     assert data is not None
@@ -12,11 +13,20 @@ def test_time_series_prs(sdat: StagyyData) -> None:
     assert (data.columns[3:6] == ["Tmin", "Tmean", "Tmax"]).all()
 
 
+def test_time_series_h5(sdat_h5: StagyyData) -> None:
+    sdat = sdat_h5
+    assert sdat.hdf5 is not None
+    data = prs.time_series_h5(sdat.hdf5 / "TimeSeries.h5", colnames=[])
+    assert data is not None
+    assert (data.columns[3:6] == ["Tmin", "Tmean", "Tmax"]).all()
+
+
 def test_time_series_invalid_prs() -> None:
-    assert prs.time_series(pathlib.Path("dummy"), []) is None
+    assert prs.time_series(Path("dummy"), []) is None
 
 
-def test_rprof_prs(sdat: StagyyData) -> None:
+def test_rprof_prs(sdat_legacy: StagyyData) -> None:
+    sdat = sdat_legacy
     names = ["aa", "bb", "cc"]
     data, time = prs.rprof(sdat.filename("rprof.dat"), list(names))
     assert all((df.columns[:3] == names).all() for df in data.values())
@@ -26,11 +36,20 @@ def test_rprof_prs(sdat: StagyyData) -> None:
     )
 
 
+def test_rprof_h5(sdat_h5: StagyyData) -> None:
+    sdat = sdat_h5
+    assert sdat.hdf5 is not None
+    data, _times = prs.rprof_h5(sdat.hdf5 / "rprof.h5", colnames=[])
+    assert data is not None
+    assert (data[1000].columns[:3] == ["r", "Tmean", "Tmin"]).all()
+
+
 def test_rprof_invalid_prs() -> None:
-    assert prs.rprof(pathlib.Path("dummy"), []) == ({}, None)
+    assert prs.rprof(Path("dummy"), []) == ({}, None)
 
 
-def test_fields_prs(sdat: StagyyData) -> None:
+def test_fields_prs(sdat_legacy: StagyyData) -> None:
+    sdat = sdat_legacy
     parsed = prs.fields(sdat.filename("t", len(sdat.snaps) - 1))
     assert parsed is not None
     hdr, flds = parsed
@@ -39,16 +58,27 @@ def test_fields_prs(sdat: StagyyData) -> None:
     assert flds.shape[1:4] == tuple(hdr["nts"])
 
 
-def test_field_header_prs(sdat: StagyyData) -> None:
+def test_field_header_prs(sdat_legacy: StagyyData) -> None:
+    sdat = sdat_legacy
     hdr = prs.field_header(sdat.filename("t", len(sdat.snaps) - 1))
     assert hdr is not None
     assert hdr["nts"].shape == (3,)
 
 
-def test_fields_istep_prs(sdat: StagyyData) -> None:
+def test_fields_istep_prs(sdat_legacy: StagyyData) -> None:
+    sdat = sdat_legacy
     istep = prs.field_istep(sdat.filename("t", len(sdat.snaps) - 1))
     assert istep == sdat.snaps[-1].istep
 
 
 def test_fields_invalid_prs() -> None:
-    assert prs.fields(pathlib.Path("dummy")) is None
+    assert prs.fields(Path("dummy")) is None
+
+
+def test_refstate_parser(example_h5_path: Path) -> None:
+    out = prs.refstate(example_h5_path / "output_refstat.dat")
+    cols = ["z", "T", "rho", "expan", "Cp", "Tcond"]
+    assert out is not None
+    systems, adias = out
+    assert (systems[0][0].columns == cols).all()
+    assert (adias[0].columns == cols).all()
